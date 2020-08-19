fixing root square for running average in RMSProp for all test functions;

python/gif_rmsprop_f1.py
@@ -66,7 +66,7 @@ e_history = [[],[]]
 
 learning_rate = 0.05
 epochs = 150
-gamma = 0.85
+gamma = 0.95
 
 # a running average of the magnitudes of recent gradients
 grad_ra_A = [0,0]
@@ -91,32 +91,32 @@ for iter in range(epochs):
     grad_ra_A[0] = gamma * grad_ra_A[0] + (1 - gamma) * (derivative_x(A[0], A[1]))**2
     grad_ra_A[1] = gamma * grad_ra_A[1] + (1 - gamma) * (derivative_y(A[0], A[1]))**2
     
-    A[0] = A[0] - learning_rate * (1/grad_ra_A[0]) * derivative_x(A[0], A[1])
-    A[1] = A[1] - learning_rate * (1/grad_ra_A[1]) * derivative_y(A[0], A[1])
+    A[0] = A[0] - learning_rate * (1/math.sqrt(grad_ra_A[0])) * derivative_x(A[0], A[1])
+    A[1] = A[1] - learning_rate * (1/math.sqrt(grad_ra_A[1])) * derivative_y(A[0], A[1])
     
     grad_ra_B[0] = gamma * grad_ra_B[0] + (1 - gamma) * (derivative_x(B[0], B[1]))**2
     grad_ra_B[1] = gamma * grad_ra_B[1] + (1 - gamma) * (derivative_y(B[0], B[1]))**2
     
-    B[0] = B[0] - learning_rate * (1/grad_ra_B[0]) * derivative_x(B[0], B[1])
-    B[1] = B[1] - learning_rate * (1/grad_ra_B[1]) * derivative_y(B[0], B[1])
+    B[0] = B[0] - learning_rate * (1/math.sqrt(grad_ra_B[0])) * derivative_x(B[0], B[1])
+    B[1] = B[1] - learning_rate * (1/math.sqrt(grad_ra_B[1])) * derivative_y(B[0], B[1])
     
     grad_ra_C[0] = gamma * grad_ra_C[0] + (1 - gamma) * (derivative_x(C[0], C[1]))**2
     grad_ra_C[1] = gamma * grad_ra_C[1] + (1 - gamma) * (derivative_y(C[0], C[1]))**2
     
-    C[0] = C[0] - learning_rate * (1/grad_ra_C[0]) * derivative_x(C[0], C[1])
-    C[1] = C[1] - learning_rate * (1/grad_ra_C[1]) * derivative_y(C[0], C[1])
+    C[0] = C[0] - learning_rate * (1/math.sqrt(grad_ra_C[0])) * derivative_x(C[0], C[1])
+    C[1] = C[1] - learning_rate * (1/math.sqrt(grad_ra_C[1])) * derivative_y(C[0], C[1])
     
     grad_ra_D[0] = gamma * grad_ra_D[0] + (1 - gamma) * (derivative_x(D[0], D[1]))**2
     grad_ra_D[1] = gamma * grad_ra_D[1] + (1 - gamma) * (derivative_y(D[0], D[1]))**2
     
-    D[0] = D[0] - learning_rate * (1/grad_ra_D[0]) * derivative_x(D[0], D[1])
-    D[1] = D[1] - learning_rate * (1/grad_ra_D[1]) * derivative_y(D[0], D[1])
+    D[0] = D[0] - learning_rate * (1/math.sqrt(grad_ra_D[0])) * derivative_x(D[0], D[1])
+    D[1] = D[1] - learning_rate * (1/math.sqrt(grad_ra_D[1])) * derivative_y(D[0], D[1])
     
     grad_ra_E[0] = gamma * grad_ra_E[0] + (1 - gamma) * (derivative_x(E[0], E[1]))**2
     grad_ra_E[1] = gamma * grad_ra_E[1] + (1 - gamma) * (derivative_y(E[0], E[1]))**2
     
-    E[0] = E[0] - learning_rate * (1/grad_ra_E[0]) * derivative_x(E[0], E[1])
-    E[1] = E[1] - learning_rate * (1/grad_ra_E[1]) * derivative_y(E[0], E[1])
+    E[0] = E[0] - learning_rate * (1/math.sqrt(grad_ra_E[0])) * derivative_x(E[0], E[1])
+    E[1] = E[1] - learning_rate * (1/math.sqrt(grad_ra_E[1])) * derivative_y(E[0], E[1])
 
 div = 10
 #for i in range(0, epochs//10):
@@ -131,4 +131,4 @@ def animate(i):
 # https://eli.thegreenplace.net/2016/drawing-animated-gifs-with-matplotlib/
 anim = FuncAnimation(fig, animate, frames=np.arange(0, epochs//div), interval=200)
     
-anim.save('images/rmsprop-f1-gamma-085.gif', dpi=80)
+anim.save('images/rmsprop-f1-gamma-095.gif', dpi=80)